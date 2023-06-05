d44ecb09f655d685cb0ac226abfca84c84a1a1a4
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-06-05 16:35:10 +0900

    Rename:modal 경로 변경

--- src/components/TravelPostDetailPage/TravelPostDetailPage.jsx
+++ src/components/TravelPostDetailPage/TravelPostDetailPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Modal from './modal';
+import Modal from '../modal/modal';
 import { useModalStore } from '../../store/store';
 
 import prevBtn from './images/prev.png';
