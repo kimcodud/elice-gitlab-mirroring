6ef1e3d577a15536714037d24d0e3db3cbcf3852
young jo <a10019610@gmail.com>
2023-06-06 10:36:17 +0900

    fix: mainpage 이미지 슬라이드 오류 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,7 +4,7 @@ import MainPage from "../pages/MainPage/MainPage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import SignUpPage from "../pages/SigUpPage/SignUpPage";
 import TravelBoardPage from "../pages/TravelBoardPage/TravelBoardPage";
-// import PlannerMap from "../components/PlannerMap/PlannerMap";
+import PlannerMap from "../components/PlannerMap/PlannerMap";
 
 const Router = () => {
   return (
@@ -14,7 +14,7 @@ const Router = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/travelBoard" element={<TravelBoardPage />} />
-        {/* <Route path="/plannerMap" element={<PlannerMap />} /> */}
+        <Route path="/plannerMap" element={<PlannerMap />} />
       </Routes>
     </BrowserRouter>
   );
