9bac1ed93f90f31ddc8732a2bf004567e8cd1499
young jo <a10019610@gmail.com>
2023-06-09 20:56:28 +0900

    feat: admin page 컴포넌트, 라우터 설정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import MainPage from '../pages/MainPage/MainPage';
-import LoginPage from '../pages/LoginPage/LoginPage';
-import SignUpPage from '../pages/SigUpPage/SignUpPage';
-import TravelBoardPage from '../pages/TravelBoardPage/TravelBoardPage';
-import Mypage from '../pages/Mypage/Mypage';
-import TravelWritePage from '../pages/TravelWritePage/TravelWritePage'
-import TravelPostDetailPage from '../pages/TravelPostDetailPage/TravelPostDetailPage'
+import MainPage from "../pages/MainPage/MainPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import SignUpPage from "../pages/SigUpPage/SignUpPage";
+import TravelBoardPage from "../pages/TravelBoardPage/TravelBoardPage";
+import Mypage from "../pages/Mypage/Mypage";
+import TravelWritePage from "../pages/TravelWritePage/TravelWritePage";
+import TravelPostDetailPage from "../pages/TravelPostDetailPage/TravelPostDetailPage";
 
-import PlannerMap from '../components/PlannerMap/PlannerMap';
-import PlannerMapEdit from '../components/PlannerMapEdit/PlannerMapEdit';
+import PlannerMap from "../components/PlannerMap/PlannerMap";
+import PlannerMapEdit from "../components/PlannerMapEdit/PlannerMapEdit";
+import AdminPage from "../pages/AdminPage/AdminPage";
 
 const Router = () => {
   return (
@@ -20,10 +21,14 @@ const Router = () => {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/travelBoard" element={<TravelBoardPage />} />
         <Route path="/Mypage" element={<Mypage />} />
-        <Route path='/travelWritePage' element={<TravelWritePage />} />
-        <Route path='/travelPostDetailPage' element={<TravelPostDetailPage />} />
+        <Route path="/travelWritePage" element={<TravelWritePage />} />
+        <Route
+          path="/travelPostDetailPage"
+          element={<TravelPostDetailPage />}
+        />
         <Route path="/plannerMap" element={<PlannerMap />} />
         <Route path="/plannerMapEdit" element={<PlannerMapEdit />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
