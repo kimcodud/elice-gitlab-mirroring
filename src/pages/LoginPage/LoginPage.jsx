62c37e2fe57c6246617d60a9975fa3c14489736e
김승환 <prpn97@gmail.com>
2023-06-12 03:10:23 +0900

    Feat : 구글, 카카오 로그인 컴포넌트

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,12 @@
 import LoginPageComponent from "../../components/LoginPage/LoginPage";
 import Google from "../../components/LoginPage/Google";
+import Kakao from "../../components/LoginPage/Kakao";
 function LoginPage() {
   return (
     <>
       <LoginPageComponent />
       <Google />
+      <Kakao />
     </>
   );
 }
