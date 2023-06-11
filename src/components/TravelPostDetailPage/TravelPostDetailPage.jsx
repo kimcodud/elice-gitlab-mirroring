73911506dc8a7dbc9a8af81428983c1512001276
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-06-11 20:02:19 +0900

    Design: 모달창 작업중

--- src/components/TravelPostDetailPage/TravelPostDetailPage.jsx
+++ src/components/TravelPostDetailPage/TravelPostDetailPage.jsx
@@ -1,165 +1,310 @@
-import { useState } from 'react';
-import Modal from '../modal/modal';
+import { useState, useEffect, useRef } from "react";
+import Modal from "../modal/modal";
 import { ModalPortal } from "../modal/ModalPortal";
-import { useModalStore } from '../../store/store';
+import { useModalStore } from "../../store/store";
 
+import prevBtn from "../../assets/prev.webp";
+import nextBtn from "../../assets/next.webp";
+import deleteBtn from "../../assets/close.webp";
 
-import prevBtn from '../../assets/prev.png';
-import nextBtn from '../../assets/next.png';
+import image1 from "./images/image-1.jpg";
+import image2 from "./images/image-2.jpg";
+import image3 from "./images/image-3.jpg";
+import image4 from "./images/image-4.jpg";
+import { commentsData, contentText, contentTitle } from "./temp";
 
-import image1 from './images/image-1.jpg';
-import image2 from './images/image-2.jpg';
-import image3 from './images/image-3.jpg';
-import image4 from './images/image-4.jpg';
-import {commentsData, contentText, contentTitle} from './temp'
-
-import userImg from '../../assets/user.png';
+import userImg from "../../assets/user.png";
 const contentImages = [image1, image2, image3, image4];
 
-
 function TravelPostDetail() {
-  const { openModal } = useModalStore();
+  const { openModal, closeModal } = useModalStore();
+  const [showComments, setShowComments] = useState(false);
+  const mainTextRef = useRef(null);
+
+  useEffect(() => {
+    document
+      .querySelector("#commentButton")
+      .addEventListener("click", openCommentModal);
+
+    return () => {
+      document
+        .querySelector("#commentButton")
+        .removeEventListener("click", openCommentModal);
+    };
+  }, []);
+
+  const CommentComponent = () => {
+    return div;
+  };
 
   const openScheduleModal = () => {
     openModal({
-      modalType: 'schedule',
+      modalType: "schedule",
       style: {
-        backgroundColor: ' #F1F5F9',
-        width: '60%',
-        height: '100%',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
+        backgroundColor: " #F1F5F9",
+        width: "60%",
+        height: "100%",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
       },
-      title: <div className='text-center font-bold text-4xl'>여행 일정</div>,
-      content: <div > 
-          <div style={{color:'red'}}>안               녕</div>
-          <div className='text-blue-400'>하세요</div>
-        </div>,
+      title: <div className="text-center font-bold text-4xl">여행 일정</div>,
+      content: (
+        <div>
+          <div style={{ color: "red" }}>안녕</div>
+          <div className="text-blue-400">하세요</div>
+        </div>
+      ),
     });
   };
 
   const openCommentModal = () => {
+    setShowComments(true);
+    mainTextRef.current.classList.add("reduced-height");
+
     openModal({
-      modalType: 'comment',
+      modalType: "comment",
       style: {
-        backgroundColor: 'green',
-        width: '40%',
-        height: '100%',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
+        backgroundColor: "rgb(249, 250, 251)",
+        width: "32.8%",
+        height: "40%",
+        top: "80%",
+        left: "67.5%",
+        transform: "translate(-50%, -50%)",
       },
-      title: '댓글보기',
-      content: tempText,
+      title: <div className="border-b border-gray-300 pb-4">댓글</div>,
+      content: (
+        <form action="" className="w-full h-full">
+          <div className="grid grid-cols-[1fr,5fr] grid-rows-[1fr,auto] w-full h-full mb-2">
+            <div className="col-span-1 row-span-full w-full p-3">
+              <div className="flex justify-center items-center w-3/4 bg-white rounded-full align-middle shadow-2xl">
+                <img
+                  className="mx-auto my-auto w-3/4"
+                  src={userImg}
+                  alt="유저이미지"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center w-full h-full">
+              <div>
+                <span className="pr-2">유저이름</span>{" "}
+                <span className="text-gray-400">2020-02-20</span>
+              </div>
+              <div>
+                <img src="deleteBtn" alt="댓글삭제" />
+              </div>
+            </div>
+            <div className="flex flex-row items-start w-full h-full">
+              댓글내용 짱짱 유익함 멋져용 와웅댓글내용 짱짱 유익함 멋져용 와웅
+              댓글내용 짱짱 유익함 멋져용 와웅댓글내용 짱짱 유익함 멋져용 와웅
+              댓글내용 짱짱 유익함 멋져용 와웅댓글내용 짱짱 유익함 멋져용 와웅
+            </div>
+          </div>
+        </form>
+      ),
+      onClose: handleCloseModal,
     });
   };
-    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleClickNext = () => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % contentImages.length); // 임시 데이터 이미지
-    };
+  const handleCloseModal = () => {
+    setShowComments(false);
+    closeModal();
+    if (mainTextRef.current) {
+      mainTextRef.current.classList.remove("reduced-height");
+    }
+  };
 
-    const handleClickPrev = () => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? contentImages.length - 1 : prevIndex - 1 // 임시 데이터 이미지
-      );
-    };
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleClickThumbnail = (index) => {
-      setCurrentIndex(index);
-    };
+  const handleClickNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % contentImages.length);
+  };
+
+  const handleClickPrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? contentImages.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleClickThumbnail = (index) => {
+    setCurrentIndex(index);
+  };
 
   return (
-    <div id="main" className="w-full h-screen flex justify-center items-center overflow-hidden" style={{height:'85vh'}}>
-  <style>
-    {`
-      .btn:hover #prevBtn,
-      .btn:hover #nextBtn {
-        display: block;
-      }
-      #Imgbox:hover #imgCategory {
-        display: flex;
-      }
-    `}
-  </style>
+    <div
+      id="main"
+      className="w-full h-screen flex justify-center items-center overflow-hidden"
+      style={{ height: "calc(100vh - 7rem)" }}
+    >
+      <style>
+        {`
+          .btn:hover #prevBtn,
+          .btn:hover #nextBtn {
+            display: block;
+          }
+          #Imgbox:hover #imgCategory {
+            display: flex;
+          }
+          .reduced-height {
+            max-height: 50%;
+          }
+          .overflow-x-uto {
+            overflow: hidden;
+            position: relative;
+          }
+  
+          .overflow-x-auto:hover {
+            overflow: auto;
+          }
+  
+          .overflow-x-auto::-webkit-scrollbar {
+            width: 8px;
+          }
+  
+          .overflow-x-auto::-webkit-scrollbar-track {
+            background-color: #F1F5F9;
+          }
+  
+          .overflow-x-auto::-webkit-scrollbar-thumb {
+            background-color: #B09FCE;
+            border-radius: 4px;
+          }
+  
+          .overflow-x-auto::-webkit-scrollbar-thumb:hover {
+            background-color: #6645B9;
+          }
+        `}
+      </style>
 
-  <div className="flex flex-row items-center gap-12 relative h-full w-2/3">
-    
-    <div className="flex flex-col w-full h-4/5">
-      <div id='Imgbox' className="flex flex-row relative" style={{ width: '100%', height: '100%' }}>
-        <div onClick={handleClickPrev}
-        className="btn bg-transparent h-full w-1/6 hover:bg-gray-200 hover:bg-opacity-20 absolute top-1/2 px-3 left-0 transform -translate-y-1/2 select-none">
-          <img id='prevBtn' className='hidden absolute top-1/2 w-1/2' style={{ transform: 'translateY(-50%)' }} src={prevBtn} alt="이전" />
-        </div>
-        <img id='mainImg' className="box-content w-full h-full object-cover rounded-2xl select-none" src={contentImages[currentIndex]} alt="Main" />  {/*임시 데이터 이미지*/}
-        <div onClick={handleClickNext}
-        className="btn bg-transparent h-full w-1/6 hover:bg-gray-200 hover:bg-opacity-20 absolute top-1/2 px-3 right-0 transform -translate-y-1/2 flex flex-row justify-end select-none">
-          <img id='nextBtn' className='hidden absolute top-1/2 w-1/2' style={{ transform: 'translateY(-50%)' }} src={nextBtn} alt="다음" />
-        </div>
-        <div id='imgCategory' className='hidden flex-row justify-center absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-200 bg-opacity-50 rounded-2xl select-none mb-3 px-3 py-1'>
-          {contentImages.map((image, index) => ( //{/* 임시 본문 이미지 */}
-            <div className={`w-${4 / contentImages.length}  p-1 hover:bg-gray-200 hover:bg-opacity-80  rounded-xl `} key={index} // 임시 데이터 이미지
-            onClick={() => handleClickThumbnail(index)}>
+      <div className="flex flex-row items-center gap-12 relative h-full w-2/3 pt-10">
+        <div className="flex flex-col w-full h-4/5">
+          <div
+            id="Imgbox"
+            className="flex flex-row relative"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <div
+              onClick={handleClickPrev}
+              className="btn bg-transparent h-full w-1/6 hover:bg-gray-200 hover:bg-opacity-20 absolute top-1/2 px-3 left-0 transform -translate-y-1/2 select-none"
+            >
               <img
-                className="box-content  w-6 h-6 rounded-xl"
-                src={image}
-                alt={`Thumbnail ${index}`}
+                id="prevBtn"
+                className="hidden absolute top-1/2 w-1/2"
+                style={{ transform: "translateY(-50%)" }}
+                src={prevBtn}
+                alt="이전"
               />
             </div>
-          ))}
+            <img
+              id="mainImg"
+              className="box-content w-full h-full object-cover rounded-2xl select-none"
+              src={contentImages[currentIndex]}
+              alt="Main"
+            />
+            <div
+              onClick={handleClickNext}
+              className="btn bg-transparent h-full w-1/6 hover:bg-gray-200 hover:bg-opacity-20 absolute top-1/2 px-3 right-0 transform -translate-y-1/2 flex flex-row justify-end select-none"
+            >
+              <img
+                id="nextBtn"
+                className="hidden absolute top-1/2 w-1/2"
+                style={{ transform: "translateY(-50%)" }}
+                src={nextBtn}
+                alt="다음"
+              />
+            </div>
+            <div
+              id="imgCategory"
+              className="hidden flex-row justify-center absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-200 bg-opacity-50 rounded-2xl select-none mb-3 px-3 py-1"
+            >
+              {contentImages.map((image, index) => (
+                <div
+                  className={`w-${
+                    4 / contentImages.length
+                  } p-1 hover:bg-gray-200 hover:bg-opacity-80 rounded-xl `}
+                  key={index}
+                  onClick={() => handleClickThumbnail(index)}
+                >
+                  <img
+                    className="box-content w-6 h-6 rounded-xl"
+                    src={image}
+                    alt={`Thumbnail ${index}`}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <div className="flex flex-col justify-between items-center  w-full h-4/5">
-      <div className="flex flex-row justify-between w-full h-20 border-b border-gray-400 py-6 px-3">
-        <div className="box-content w-full text-3xl font-bold" style={{color: '#6645B9'}}>
-        <p>{contentTitle}</p> {/* 임시 제목 */}
-        </div>
-        <button onClick={openScheduleModal} className="rounded-lg p-2 w-28 h-10 text-white" style={{ backgroundColor: '#B09FCE'}}>
-          일정보기
-        </button>
-      </div>
-      <div id="mainText" className="whitespace-pre-wrap overflow-x-auto overflow-scroll w-full h-full px-5 text-xl m-5">
-        {contentText} {/* 임시 본문 */}
-      </div>
-      <div className="flex flex-row justify-between  w-full border-t border-gray-400 pt-5 pb-3 px-3 ">
-        <button onClick={openCommentModal} style={{ alignSelf: 'flex-start' }}>
-          댓글보기
-        </button>
-        <div>
-          작성일 : 2020-02-20
-          <div className="flex flex-row justify-end">
-            <button style={{ display: 'none' }}>수정</button>
-            <button style={{ display: 'none' }} className="ml-3">
-              삭제
+        <div className="flex flex-col items-center w-full h-4/5">
+          <div className="flex flex-row justify-between w-full h-20 border-b border-gray-400 py-6 px-3">
+            <div
+              className="box-content w-full text-3xl font-bold"
+              style={{ color: "#6645B9" }}
+            >
+              <p>{contentTitle}</p>
+            </div>
+            <button
+              onClick={openScheduleModal}
+              className="rounded-lg p-2 w-28 h-10 text-white"
+              style={{ backgroundColor: "#B09FCE" }}
+            >
+              일정보기
             </button>
+          </div>
+          <div
+            id="mainText"
+            ref={mainTextRef}
+            className={`whitespace-pre-wrap overflow-x-auto overflow-y-hidden overflow-scroll w-full h-full p-5 border-b border-gray-400 ${
+              showComments ? "reduced-height" : ""
+            }`}
+          >
+            {contentText}
+          </div>
+
+          <div className="flex flex-row justify-between w-full pt-5 pb-3 px-3">
+            <button
+              id="commentButton"
+              onClick={openCommentModal}
+              style={{ alignSelf: "flex-start" }}
+            >
+              댓글보기
+            </button>
+            <div>
+              작성일 : 2020-02-20
+              <div className="flex flex-row justify-end">
+                <button style={{ display: "none" }}>수정</button>
+                <button style={{ display: "none" }} className="ml-3">
+                  삭제
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            id="commentBox"
+            className="grid grid-cols-[1fr,10fr,1.5fr] gap-2 justify-items-center items-center bg-gray-100 w-full p-3 mt-3 rounded-2xl bottom-0"
+          >
+            <div className="box-content bg-white h-8 w-8 rounded-full align-middle shadow-2xl">
+              <img src={userImg} alt="유저이미지" />
+            </div>
+            <input
+              type="text"
+              placeholder="댓글을 작성해주세요."
+              className="bg-transparent justify-self-center w-full h-8 px-4 bg-white rounded-2xl hide-input-focus outline-none"
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="box-content text-white rounded w-16 py-1 mr-2 cursor-pointer"
+              style={{ backgroundColor: "#B09FCE" }}
+            />
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-[1fr,10fr,1.5fr] gap-2 justify-items-center items-center bg-gray-100 w-full p-3 mt-3 rounded-2xl bottom-0">
-        <div className="box-content bg-white h-8 w-8 rounded-full align-middle shadow-2xl">
-          <img src={userImg} alt="유저이미지" />
-        </div>
-        <input
-          type="text"
-          placeholder={'댓글을 작성해주세요.'}
-          className="bg-transparent justify-self-center w-full h-8 px-4 bg-white rounded-2xl hide-input-focus outline-none"
-        />
-        <input
-          type="submit"
-          value="입력"
-          className="box-content text-white rounded w-16 py-1 mr-2 cursor-pointer" style={{ backgroundColor: '#B09FCE'}}
-        />
-      </div>
+      <ModalPortal>
+        <Modal onClose={handleCloseModal} />
+      </ModalPortal>
     </div>
-    <ModalPortal>
-      <Modal />
-    </ModalPortal>
-  </div>
-</div>
-
   );
 }
 
