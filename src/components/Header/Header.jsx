70342ef647b71e99bbcf3eb2416117d948ac8c3e
young jo <a10019610@gmail.com>
2023-06-12 13:54:17 +0900

    feat: user role에 따라 nav 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Header = () => {
+  const [role, setRole] = useState("");
+  const roleCheck = () => {
+    if (localStorage.getItem("role")) {
+      setRole(localStorage.getItem("role"));
+    }
+  };
+
+  const logoutHandler = () => {
+    const res = axios.post("http://localhost:3000/users/logout");
+    console.log(res);
+    localStorage.removeItem("role");
+  };
+
+  useEffect(() => {
+    roleCheck();
+    // logoutHandler()
+  }, []);
   return (
     <header className="h-14">
       <nav className="p-1 flex justify-start h-14">
         <a href="/">
           <img
             src="/src/assets/starRoad_2.png"
-            className="ml-6 mr-96 h-20 w-18 -mt-3"
+            className="ml-6 mr-48 h-20 w-18 -mt-3"
           ></img>
         </a>
         <a href="/plannerMap" className="text-zinc-400 ml-auto p-3">
@@ -17,18 +36,36 @@ const Header = () => {
         <a href="/plannerEdit" className="text-zinc-400 ml-auto p-3">
           일정 수정(임시)
         </a>
+        {role === "ADMIN" && (
+          <a href="/admin" className="text-violet-600 font-bold ml-auto p-3">
+            ADMIN
+          </a>
+        )}
         <a
           href="/travelBoard"
           className="text-zinc-400 ml-auto p-3 bg-white hover:bg-gray-100"
         >
           여행기
         </a>
-        <a
-          href="/login"
-          className="text-zinc-400 p-3 bg-white hover:bg-gray-100"
-        >
-          로그인
-        </a>
+        {!role && (
+          <a
+            href="/login"
+            className="text-zinc-400 p-3 bg-white hover:bg-gray-100"
+          >
+            로그인
+          </a>
+        )}
+        {/* {role && (
+          <button onClick={logoutHandler}>
+            <a
+              href="/"
+              className="text-zinc-400 p-3 bg-white hover:bg-gray-100"
+            >
+              로그아웃
+            </a>
+          </button>
+        )} */}
+        <button onClick={logoutHandler}>로그아웃</button>
         <a
           href="/signup"
           className="text-zinc-400 mr-5 p-3 bg-white hover:bg-gray-100"
