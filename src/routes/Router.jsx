67c13d95130b8881a23bb3354d35ab99e33e323a
young jo <a10019610@gmail.com>
2023-06-14 07:35:38 +0900

    fix: travelWritepage 라우터 id 파라미터 경로 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -53,7 +53,7 @@ const Router = () => {
         <>
           <Route path="/travelBoard" element={<TravelBoardPage />} />
           <Route path="/Mypage" element={<Mypage />} />
-          <Route path="/travelWritePage" element={<TravelWritePage />} />
+          <Route path="/travelWritePage:postId" element={<TravelWritePage />} />
           <Route
             path="/TravelPostDetailPage/:postId"
             element={<TravelPostDetailPage />}
