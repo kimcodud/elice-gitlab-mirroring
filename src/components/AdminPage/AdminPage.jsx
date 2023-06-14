e1a2b1ab8dd6ce2c616062cd6d58aa137766c237
young jo <a10019610@gmail.com>
2023-06-15 03:46:46 +0900

    fix: router 분기 처리 수정

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -71,102 +71,113 @@ const AdminPageComponent = () => {
       console.log("API 호출 중 오류가 발생했습니다:", error);
     }
   };
+  // const deleteHandler = () => {
+  //   const result = axios.delete(`http://localhost:3000/admin/locations/${}`);
+  // }
 
   return (
-    <form
-      className="max-w-md mx-auto mt-10 p-4 bg-slate-50 shadow-xl rounded-lg"
-      onSubmit={handleSubmit}
-    >
-      <div className="mb-4">
-        <label htmlFor="image" className="block text-gray-700 font-bold mb-2">
-          여행지 이미지 등록
-        </label>
-        <input
-          type="file"
-          id="image"
-          accept="image/*"
-          onChange={handleImageChange}
-          className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="nameEn" className="block text-gray-700 font-bold mb-2">
-          영문 이름
-        </label>
-        <input
-          type="text"
-          id="nameEn"
-          value={name_en}
-          onChange={handleNameEnChange}
-          className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="nameKo" className="block text-gray-700 font-bold mb-2">
-          한글 이름
-        </label>
-        <input
-          type="text"
-          id="nameKo"
-          value={name_ko}
-          onChange={handleNameKoChange}
-          className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-        />
-      </div>
-      <div className="mb-4">
-        <span className="flex">
-          <label
-            htmlFor="nameKo"
-            className="block text-gray-700 font-bold mb-2 mr-48"
-          >
-            위도
+    <div>
+      <form
+        className="max-w-md mx-auto mt-10 p-4 bg-slate-50 shadow-xl rounded-lg"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label htmlFor="image" className="block text-gray-700 font-bold mb-2">
+            여행지 이미지 등록
           </label>
+          <input
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="nameEn"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            영문 이름
+          </label>
+          <input
+            type="text"
+            id="nameEn"
+            value={name_en}
+            onChange={handleNameEnChange}
+            className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
+          />
+        </div>
+        <div className="mb-4">
           <label
             htmlFor="nameKo"
             className="block text-gray-700 font-bold mb-2"
           >
-            경도
+            한글 이름
           </label>
-        </span>
-        <span style={{ display: "flex" }}>
           <input
             type="text"
             id="nameKo"
-            value={latitude}
-            onChange={handleLatitudeChange}
-            className="border rounded px-3 py-2 w-48 mr-5  focus:outline-none focus:ring-2 focus:ring-purple-500"
+            value={name_ko}
+            onChange={handleNameKoChange}
+            className="border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500"
           />
+        </div>
+        <div className="mb-4">
+          <span className="flex">
+            <label
+              htmlFor="nameKo"
+              className="block text-gray-700 font-bold mb-2 mr-48"
+            >
+              위도
+            </label>
+            <label
+              htmlFor="nameKo"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              경도
+            </label>
+          </span>
+          <span style={{ display: "flex" }}>
+            <input
+              type="text"
+              id="nameKo"
+              value={latitude}
+              onChange={handleLatitudeChange}
+              className="border rounded px-3 py-2 w-48 mr-5  focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
 
-          <input
-            type="text"
-            id="nameKo"
-            value={longitude}
-            onChange={handleLongitudeChange}
-            className="border rounded px-3 w-full py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+            <input
+              type="text"
+              id="nameKo"
+              value={longitude}
+              onChange={handleLongitudeChange}
+              className="border rounded px-3 w-full py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
+          </span>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="introduction"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            소개:
+          </label>
+          <textarea
+            id="introduction"
+            value={introduction}
+            onChange={handleIntroductionChange}
+            className="border rounded px-3 py-2 w-full h-32 resize-none focus:outline-none focus:ring-2 focus:ring-purple-500"
           />
-        </span>
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="introduction"
-          className="block text-gray-700 font-bold mb-2"
+        </div>
+        <button
+          type="submit"
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
         >
-          소개:
-        </label>
-        <textarea
-          id="introduction"
-          value={introduction}
-          onChange={handleIntroductionChange}
-          className="border rounded px-3 py-2 w-full h-32 resize-none focus:outline-none focus:ring-2 focus:ring-purple-500"
-        />
-      </div>
-      <button
-        type="submit"
-        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-      >
-        등록
-      </button>
-    </form>
+          등록
+        </button>
+      </form>
+    </div>
   );
 };
 
